d629506f15465223b231e08dab868d203f032a30
Joao Paulo <j.santos@diagno.ind.br>
2020-01-13 14:00:19 -0300

    cria pastas próprias para cada id

--- codigos/main.py
+++ codigos/main.py
@@ -3,17 +3,29 @@ import glob
 import painter as pnt
 import reproducibility as rpd
 
-data_path = '../dados/'
-results_path = '../resultados/'
+os.chdir("../")
+data_path = 'dados/'
+base_results_path = 'resultados/'
 
-os.chdir(data_path)
-measures   = glob.glob('*results.csv')
-pulses_plt_files = glob.glob('*plt.csv')
-pulses_wbc_files = glob.glob('*wbc.csv')
-pulses_rbc_files = glob.glob('*rbc.csv')
-basename = measures[0][:10]
+data_directories = glob.glob(data_path + '*/')
 
-rpd.reproducibility_table(measures[0],basename,results_path)
-pnt.create_histogram(pulses_plt_files,"plt",basename,results_path)
-pnt.create_histogram(pulses_wbc_files,"wbc",basename,results_path)
-pnt.create_histogram(pulses_rbc_files,"rbc",basename,results_path)
+for data_directory in data_directories:
+    basename = data_directory.split("/")[1]
+    measures   = glob.glob(data_directory + '*results.csv')
+    pulses_plt_files = glob.glob(data_directory + '*plt.csv')
+    pulses_wbc_files = glob.glob(data_directory + '*wbc.csv')
+    pulses_rbc_files = glob.glob(data_directory + '*rbc.csv')
+    if not measures:
+        print("Arquivo de resultados nao encontrado")
+        next
+    
+    results_path = base_results_path + basename + "/"
+    try: 
+        os.mkdir(results_path) 
+    except OSError as error: 
+        print("Diretorio {} nao foi criado pois ja existe".format(results_path))     
+    
+    rpd.reproducibility_table(measures[0],basename,results_path)
+    pnt.create_histogram(pulses_plt_files,"plt",basename,results_path)
+    pnt.create_histogram(pulses_wbc_files,"wbc",basename,results_path)
+    pnt.create_histogram(pulses_rbc_files,"rbc",basename,results_path)
