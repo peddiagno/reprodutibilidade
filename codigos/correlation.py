77468fe3e5ca0be9c32652d019cdce81b8fbbe54
Joao Paulo <j.santos@diagno.ind.br>
2020-01-16 08:51:55 -0300

    faz correlacao e salva em planilha

--- codigos/correlation.py
+++ codigos/correlation.py
@@ -1,11 +1,9 @@
 import pandas as pd
 def correlation_table(exams_path, results_path):
     exam_dataframes = []
-    for exam in exams_path:
-        exam_dataframes.append( pd.read_csv(exam, na_values = "?", comment='\t',
+    for i in range(2):
+        exam_dataframes.append( pd.read_csv( exams_path[i], na_values = "?", comment='\t',
             sep=",", skipinitialspace=True ) )
-    2+2
-
     headers = exam_dataframes[0].columns
     correlation_values = []
     for col in headers:
@@ -20,8 +18,27 @@ def correlation_table(exams_path, results_path):
     correlation_table = correlation_table.round(2)
     
     writer = pd.ExcelWriter(results_path + "correlacao.xlsx", engine = 'xlsxwriter')
+    serial_number0 = exams_path[0].split("/")[2].split("_")[0]
+    serial_number1 = exams_path[1].split("/")[2].split("_")[0]
     
-    exam_dataframes[0].to_excel(writer, sheet_name='Sheet1')
-    exam_dataframes[1].to_excel(writer, sheet_name='Sheet2')
-    correlation_table.to_excel(writer, sheet_name='Correlacao')
+
+    workbook = writer.book
+    title_format = workbook.add_format({'bold': True})
+    
+    prepare_sheets(exam_dataframes[0], serial_number0,
+                   "Exames {}".format(serial_number0), writer, title_format )
+    
+    prepare_sheets(exam_dataframes[1], serial_number1,
+                   "Exames {}".format(serial_number1), writer, title_format )
+    
+    prepare_sheets(correlation_table, "Correlacao",
+                   "Correlacao entre os equipamentos", writer, title_format )
+
     writer.save()
+    
+def prepare_sheets(dataframe, sheet_name, title, excel_writer, style):
+
+    dataframe.to_excel(excel_writer, sheet_name=sheet_name,
+                   startcol = 0, startrow = 1, index = False)
+    worksheet = excel_writer.sheets[sheet_name]
+    worksheet.write('I1', title, style)
